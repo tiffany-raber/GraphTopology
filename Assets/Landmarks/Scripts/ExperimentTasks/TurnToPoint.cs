470d97d07ecebba61f0275ceada027af477a445d
Mike Starrett Ambrose <mjstarrett@gmail.com>
2024-01-30 19:29:42 -0800

    simple pointing task coming together - borrow more from compassPointing.cs

--- Assets/Landmarks/Scripts/ExperimentTasks/TurnToPoint.cs
+++ Assets/Landmarks/Scripts/ExperimentTasks/TurnToPoint.cs
@@ -1,13 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static System.Runtime.CompilerServices.RuntimeHelpers;
 
 public class TurnToPoint : ExperimentTask
 {
     [Header("Task-specific Properties")]
-    [Min(0)] public float timeAllotted_s;
     private float onset;
+    private float responseMovementOnset;
     private float responseLatency;
+    public KeyCode  ccwButton = KeyCode.LeftArrow;
+    public KeyCode cwButton = KeyCode.RightArrow;
+    public KeyCode submitButton = KeyCode.UpArrow;
+    public int rotationSpeedMultiplier = 1;
+    public bool useBodyNotCameraForRotation;
+    private Transform rotationSource;
+    private float lastFrameY;
+    private float initialY; 
+    private float totalClockwiseRotation; 
+    private float finalY; 
+    private bool hasMoved; // has the player started adjusting the facing direction yet?
+    private bool responded;
+    private float signedError;
+    private float absoluteError;
+
+    
 
     public override void startTask()
     {
@@ -29,17 +46,41 @@ public class TurnToPoint : ExperimentTask
         }
 
         // WRITE TASK STARTUP CODE HERE
+        if (useBodyNotCameraForRotation) rotationSource = avatar.GetComponent<LM_PlayerController>().collisionObject.transform;
+        else rotationSource = avatar.GetComponent<LM_PlayerController>().cam.transform;
+        initialY = rotationSource.eulerAngles.y;
+        totalClockwiseRotation = 0;
+
         onset = -1f;
     }
 
 
     public override bool updateTask()
     {
+        // keep time
         if (onset < 0) onset = Time.time;
         responseLatency = Time.time - onset;
 
-        if (Input.GetKeyDown(KeyCode.Return))
+        // Keep track of rotations as we go
+        totalClockwiseRotation += rotationSource.eulerAngles.y - lastFrameY;
+        lastFrameY = rotationSource.eulerAngles.y;
+        if (totalClockwiseRotation > 0 && !hasMoved)
         {
+            responseMovementOnset = Time.time;
+            hasMoved = true;
+        }
+
+        // Handle the response input 
+        if (Input.GetKeyDown(submitButton) && !responded)
+        {
+            finalY = rotationSource.eulerAngles.y;
+            responded = true;
+            if (interval == 0) return true; // end with response unless duration/interval is specified
+        }
+        // If we're using a fixed duration just end when times up (regardless of response status
+        if (interval > 0 && Time.time - onset > 0)
+        {
+            finalY = rotationSource.eulerAngles.y; // still record if time ran out
             return true;
         }
 
@@ -59,11 +100,21 @@ public class TurnToPoint : ExperimentTask
     {
         base.endTask();
 
-        // WRITE TASK EXIT CODE HERE
-        taskLog.AddData(transform.name + "_onset_s", onset.ToString());
-        taskLog.AddData(transform.name + "_duration_s", timeAllotted_s.ToString());
-        taskLog.AddData(transform.name + "_responseLatency", responseLatency.ToString());
+        // Calculations
 
+        // Logging
+        taskLog.AddData(transform.name + "event_onset_s", onset.ToString());
+        taskLog.AddData(transform.name + "mvmt_onset_s", responseMovementOnset.ToString());
+        if (interval != 0) taskLog.AddData(transform.name + "_duration_s", (interval / 1000).ToString());
+        else taskLog.AddData(transform.name + "_duration_s", responseLatency.ToString());
+        taskLog.AddData(transform.name + "_responseLatency", responseLatency.ToString());
+        taskLog.AddData(transform.name + "_initialY", initialY.ToString());
+        taskLog.AddData(transform.name + "_finalY", finalY.ToString());
+        taskLog.AddData(transform.name + "_responded", responded.ToString());
+        taskLog.AddData(transform.name + "_signedError", signedError.ToString());
+        taskLog.AddData(transform.name + "_absError", absoluteError.ToString());
+
+        // Clean up
     }
 
 }
