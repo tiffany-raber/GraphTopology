af97ce4cbf732bfe485617b261183c2242ae5b84
Mike Starrett Ambrose <mjstarrett@gmail.com>
2024-02-01 20:01:33 -0800

    FP_pointing close to ready; TODO - some logging and optional target prompt

--- Assets/Landmarks/Scripts/ExperimentTasks/TurnToPoint.cs
+++ Assets/Landmarks/Scripts/ExperimentTasks/TurnToPoint.cs
@@ -23,11 +23,11 @@ public class TurnToPoint : ExperimentTask
     private float lastFrameY;
     private float initialGlobalY; 
     private float initialLocalY;
-    private float totalClockwiseRotation; 
+    private float totalRotation; 
+    private float netClockwiseRotation;
     private bool hasMoved; // has the player started adjusting the facing direction yet?
     private float finalGlobalY; 
     private float finalLocalY;
-    private float correctGlobalY;
     private float correctLocalY;
     private bool responded;
     private float signedError;
@@ -56,7 +56,8 @@ public class TurnToPoint : ExperimentTask
         if (useBodyNotCameraForRotation) rotationSource = avatar.GetComponent<LM_PlayerController>().collisionObject.transform;
         else rotationSource = avatar.GetComponent<LM_PlayerController>().cam.transform;
         lastFrameY = Single.NaN;
-        totalClockwiseRotation = 0;
+        totalRotation = 0;
+        netClockwiseRotation = 0;
         onset = -1f;
         responded = false;
         hasMoved =false;
@@ -87,13 +88,14 @@ public class TurnToPoint : ExperimentTask
         responseLatency = Time.time - onset;
 
         // Keep track of rotations as we go
-        if (lastFrameY == Single.NaN)
-        {
-            totalClockwiseRotation += rotationSource.eulerAngles.y - lastFrameY;
-            lastFrameY = rotationSource.eulerAngles.y;
-        }
+        if (lastFrameY == Single.NaN) lastFrameY = rotationSource.eulerAngles.y;
+        var deltaY = Mathf.DeltaAngle(rotationSource.eulerAngles.y, lastFrameY);
+        totalRotation += deltaY;
+        if (rotationSource.eulerAngles.y < lastFrameY) netClockwiseRotation -= deltaY;
+        else netClockwiseRotation += deltaY;
+        lastFrameY = rotationSource.eulerAngles.y;
        
-        if (totalClockwiseRotation > 0 && !hasMoved)
+        if (totalRotation > 0 && !hasMoved)
         {
             responseMovementOnset = Time.time;
             hasMoved = true;
@@ -103,9 +105,13 @@ public class TurnToPoint : ExperimentTask
         if (Input.GetKeyDown(submitButton) && !responded)
         {
             finalGlobalY = rotationSource.eulerAngles.y;
+            finalLocalY = Experiment.CalculateAngleThreePoints( currentHeading.transform.position, 
+                                                                currentOrigin.transform.position, 
+                                                                currentOrigin.transform.position + currentOrigin.transform.forward);
             responded = true;
             if (interval == 0) return true; // end with response unless duration/interval is specified
         }
+
         // If we're using a fixed duration just end when times up (regardless of response status
         if (interval > 0 && Time.time - onset > 0)
         {
@@ -113,7 +119,7 @@ public class TurnToPoint : ExperimentTask
             finalLocalY = Experiment.CalculateAngleThreePoints( currentHeading.transform.position, 
                                                                 currentOrigin.transform.position, 
                                                                 currentOrigin.transform.position + currentOrigin.transform.forward);
-            
+            responded = false;
             return true;
         }
 
@@ -135,20 +141,31 @@ public class TurnToPoint : ExperimentTask
 
         // Calculations
         Debug.Log(finalLocalY);
+        absoluteError = Mathf.DeltaAngle(finalLocalY, correctLocalY);
+        Debug.Log("Absolute error " + absoluteError);
+        // FIXME FIXME - calculate signed error
+        var underEstimated = Mathf.Abs(finalLocalY) < Mathf.Abs(correctLocalY);
+        signedError = underEstimated?-1*absoluteError:absoluteError;
         
         // Logging
         taskLog.AddData(transform.name + "event_onset_s", onset.ToString());
         taskLog.AddData(transform.name + "mvmt_onset_s", responseMovementOnset.ToString());
         if (interval != 0) taskLog.AddData(transform.name + "_duration_s", (interval / 1000).ToString());
-        else taskLog.AddData(transform.name + "_duration_s", responseLatency.ToString());
+        else taskLog.AddData(transform.name + "_duration_s", "todo_FIXME"); // fixme event_duration and respDuration
         taskLog.AddData(transform.name + "_responseLatency", responseLatency.ToString());
         taskLog.AddData(transform.name + "_initialY_allo", initialGlobalY.ToString());
         taskLog.AddData(transform.name + "_initialY_ego", initialLocalY.ToString());
+        taskLog.AddData(transform.name + "_totalRotation", totalRotation.ToString());
+        taskLog.AddData(transform.name + "_netCWrotation", netClockwiseRotation.ToString());
         taskLog.AddData(transform.name + "_finalY_allo", finalGlobalY.ToString());
         taskLog.AddData(transform.name + "_finalY_ego", finalLocalY.ToString());
+        taskLog.AddData(transform.name + "_correctY_ego", correctLocalY.ToString());
         taskLog.AddData(transform.name + "_responded", responded.ToString());
+        taskLog.AddData(transform.name + "_overUnder", underEstimated?"under":"over");
         taskLog.AddData(transform.name + "_signedError", signedError.ToString());
         taskLog.AddData(transform.name + "_absError", absoluteError.ToString());
+        taskLog.AddData(transform.name + "_trialType", "todo_string_stayswitch");
+        taskLog.AddData(transform.name + "_stayCount", "todo_integer_trialsSinceLastSwitch");
 
         // Clean up
         if (canIncrementLists)
